f92d35584fd265a9e34a7bd0a3be67280dd657a1
ZoneTwelve <zonetwelve159@gmail.com>
2016-08-12 14:02:11 +0800

    Add files via upload

---
+++ PHP/PHP-simple-update/upload.php
@@ -0,0 +1,10 @@
+<?php
+$updir = './files';
+$upfiles = $updir.basename($_FILES['upfile']['name']);
+if (move_uploaded_file($_FILES['upfile']['tmp_name'], $upfiles)) {
+ echo "document.location.href="./update.html;<h1>上傳成功 \n";
+} else {
+ echo "document.location.href="./update.html";<h1>上傳失敗 \n";
+}
+print_r($_FILES);
+?>
