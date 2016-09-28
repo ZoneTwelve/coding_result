d68cf8ddc08744a22f24eebefa23dc240000019a
ZoneTwelve <zonetwelve159@gmail.com>
2016-09-29 07:32:19 +0800

    Update Multiplication_Table.js

--- NodeJS/Multiplication-table/Multiplication_Table.js
+++ NodeJS/Multiplication-table/Multiplication_Table.js
@@ -1,2 +1,2 @@
 var num = [1,2,3,4,5,6,7,8,9];
-for(var i=0;i<num.length;i++)for(var j=0;j<num.length;j++)		console.log(num[i]+"	*	"+num[j]+"	=	"+num[i]*num[j])
+for(var i=0;i<num.length;i++)for(var j=0;j<num.length;j++)console.log(num[i]+"	*	"+num[j]+"	=	"+num[i]*num[j])
