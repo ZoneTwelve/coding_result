d56ef2af7d21bbd0c65ce09a72ac14e80b0f608f
ZoneTwelve <zonetwelve159@gmail.com>
2016-10-17 11:36:00 +0800

    Update reset.php

--- PHP/PHP_register-JSON/reset.php
+++ PHP/PHP_register-JSON/reset.php
@@ -1,6 +1,6 @@
 <?php
 $data = array();   
-$data["0"] = array ("name"=>"Me", "tel"=>"0912345678", "address"=> "新北");   
+$data["0"] = array ("name"=>"ZoneTwelve", "tel"=>"0912345678", "address"=> "新北");   
 $data["1"] = array ("name"=>"Myself", "tel"=>"0987654321", "address"=> "聖荷西"); 
 $json_string = json_encode($data);
 file_put_contents('userdb.json', $json_string);
