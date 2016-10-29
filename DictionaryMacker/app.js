bf57053dc957f42641ac00382075dba3a126d346
ZoneTwelve <zonetwelve159@gmail.com>
2016-10-29 15:59:03 +0800

    Add files via upload

--- DictionaryMacker/app.js
+++ DictionaryMacker/app.js
@@ -1,27 +1,29 @@
+//modules
 var fs = require("fs");
 
+//setting
 var filename ="test.txt";
-var need = 3;
-var myArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',"0","1","2","3","4","5","6","7","8","9"];
+var need = 8;
+var string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()_+`-=";
 
+
+var myArray = string.split("");
 var dimensions = [];
+var result = [];
 for (var i=0;i<need;i++){
 	dimensions.push(myArray);
 }
-
-var result = [];
-
 function explore(curDim, prefix) {
 	var nextDim = dimensions.shift();
 	for (var i = 0; i < curDim.length; i++) {
-		if (nextDim) 
+		if (nextDim)
 			explore(nextDim, prefix + curDim[i]);
-		else 
+		else
 			result.push(prefix + curDim[i]);
 	}
 	if (nextDim) dimensions.push(nextDim);
 }
 explore(dimensions.shift(), "");
 
-//console.log(result)
-fs.writeFile(filename,result.join("\r\n"),console.log("完成"));
+console.log(result)
+//fs.writeFile(filename,result.join("\r\n"),console.log("完成"));
