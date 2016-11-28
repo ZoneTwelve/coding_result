a1b8396de593fadc03acf0b17e89cb2e37ba976b
ZoneTwelve <zonetwelve159@gmail.com>
2016-11-29 00:49:05 +0800

    Update upload.php

--- PHP/PHP-simple-update/upload.php
+++ PHP/PHP-simple-update/upload.php
@@ -4,7 +4,7 @@ $upfiles = $updir.basename($_FILES['upfile']['name']);
 if (move_uploaded_file($_FILES['upfile']['tmp_name'], $upfiles)) {
  echo "document.location.href=./update.html;<h1>上傳成功 \n";
 } else {
- echo "document.location.href="./update.html";<h1>上傳失敗 \n";
+ echo "document.location.href='/update.html';<h1>上傳失敗 \n";
 }
 print_r($_FILES);
 ?>
